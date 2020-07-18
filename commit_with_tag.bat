9b3928e4ac873f853299f31ce6b6f0dd82894c5a
Andreas Junemann <ajunemann@outlook.de>
2020-07-18 13:03:03 +0200

    v1.1.3

--- commit_with_tag.bat
+++ commit_with_tag.bat
@@ -1,10 +1,7 @@
-
-
-
-
 for /f %i in ('python version.py -v %VERSION%') do set VERSION=%i
 
 git add --all
 git commit -m "%VERSION% "
+git tag -a "%VERSION" -m "Release %VERSION%"
 git push origin master
 git push origin %VERSION%
